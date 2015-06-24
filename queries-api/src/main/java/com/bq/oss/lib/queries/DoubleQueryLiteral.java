bc417d7130fac509fdd298061fbb769653a13424
Rubén Carrasco <rcarrasco@bq.com>
2015-06-24 15:25:13 +0200

    complete builder with all type of nodes
    Change-Id: I878908805ade7aae1c7884339e725cb624cda818

--- queries-api/src/main/java/com/bq/oss/lib/queries/DoubleQueryLiteral.java
+++ queries-api/src/main/java/com/bq/oss/lib/queries/DoubleQueryLiteral.java
@@ -3,38 +3,46 @@ package com.bq.oss.lib.queries;
 
 public class DoubleQueryLiteral extends NumericQueryLiteral<Double> {
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((literal == null) ? 0 : literal.hashCode());
-		return result;
-	}
+    public DoubleQueryLiteral() {
+        super();
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		DoubleQueryLiteral other = (DoubleQueryLiteral) obj;
-		if (literal == null) {
-			if (other.literal != null) {
-				return false;
-			}
-		} else if (!literal.equals(other.literal)) {
-			return false;
-		}
-		return true;
-	}
+    public DoubleQueryLiteral(Double literal) {
+        super(literal);
+    }
 
-	@Override
-	public String toString() {
-		return Double.toString(getLiteral());
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((literal == null) ? 0 : literal.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        DoubleQueryLiteral other = (DoubleQueryLiteral) obj;
+        if (literal == null) {
+            if (other.literal != null) {
+                return false;
+            }
+        } else if (!literal.equals(other.literal)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(getLiteral());
+    }
 }
