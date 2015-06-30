aa7a9d84480cdbb4f6a935d9d837cc46d17d57e3
delCerroCristian <cristian.delcerro@bq.com>
2015-06-30 10:45:07 +0200

    Add  operator
    Change-Id: I36964bbbcd90f98037eae5f5f6d2a4ab69e2630f

--- queries-api/src/main/java/com/bq/oss/lib/queries/request/QueryLiteral.java
+++ queries-api/src/main/java/com/bq/oss/lib/queries/request/QueryLiteral.java
@@ -5,103 +5,106 @@ import com.bq.oss.lib.queries.matcher.QueryMatcher;
 
 public abstract class QueryLiteral<T> {
 
-	protected T literal;
+    protected T literal;
 
-	public QueryLiteral() {
-	}
+    public QueryLiteral() {}
 
-	public QueryLiteral(T literal) {
-		this.literal = literal;
-	}
+    public QueryLiteral(T literal) {
+        this.literal = literal;
+    }
 
-	public T getLiteral() {
-		return literal;
-	}
+    public T getLiteral() {
+        return literal;
+    }
 
-	public void setLiteral(T literal) {
-		this.literal = literal;
-	}
+    public void setLiteral(T literal) {
+        this.literal = literal;
+    }
 
-	public boolean operate(QueryOperator operator, Object object) throws QueryMatchingException {
-		return operate(operator, object, null);
-	}
+    public boolean operate(QueryOperator operator, Object object) throws QueryMatchingException {
+        return operate(operator, object, null);
+    }
 
-	public boolean operate(QueryOperator operator, Object object, QueryMatcher queryMatcher)
-			throws QueryMatchingException {
+    public boolean operate(QueryOperator operator, Object object, QueryMatcher queryMatcher) throws QueryMatchingException {
 
-		try {
-			switch (operator) {
-				case $ALL:
-					return this.all(object);
-				case $ELEM_MATCH:
-					return this.elemMatch(object, queryMatcher);
-				case $EQ:
-					return this.eq(object);
-				case $GT:
-					return this.gt(object);
-				case $GTE:
-					return this.gte(object);
-				case $IN:
-					return this.in(object);
-				case $LIKE:
-					return this.like(object);
-				case $LT:
-					return this.lt(object);
-				case $LTE:
-					return this.lte(object);
-				case $NE:
-					return this.ne(object);
-				default:
-					return false;
-			}
-		} catch (ClassCastException | NumberFormatException e) {
-			throw new QueryMatchingException("The literal and the object type not matches, ", e);
-		}
-	}
+        try {
+            switch (operator) {
+                case $ALL:
+                    return this.all(object);
+                case $ELEM_MATCH:
+                    return this.elemMatch(object, queryMatcher);
+                case $EQ:
+                    return this.eq(object);
+                case $GT:
+                    return this.gt(object);
+                case $GTE:
+                    return this.gte(object);
+                case $IN:
+                    return this.in(object);
+                case $NIN:
+                    return this.nin(object);
+                case $LIKE:
+                    return this.like(object);
+                case $LT:
+                    return this.lt(object);
+                case $LTE:
+                    return this.lte(object);
+                case $NE:
+                    return this.ne(object);
+                default:
+                    return false;
+            }
+        } catch (ClassCastException | NumberFormatException e) {
+            throw new QueryMatchingException("The literal and the object type not matches, ", e);
+        }
+    }
 
-	protected boolean eq(Object object) throws QueryMatchingException {
-		throw new QueryMatchingException(getExceptionMessage(QueryOperator.$EQ));
-	}
+    protected boolean eq(Object object) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$EQ));
+    }
 
-	protected boolean all(Object object) throws QueryMatchingException {
-		throw new QueryMatchingException(getExceptionMessage(QueryOperator.$ALL));
-	}
+    protected boolean all(Object object) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$ALL));
+    }
 
-	protected boolean elemMatch(Object object, QueryMatcher queryMatcher) throws QueryMatchingException {
-		throw new QueryMatchingException(getExceptionMessage(QueryOperator.$ELEM_MATCH));
-	}
+    protected boolean elemMatch(Object object, QueryMatcher queryMatcher) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$ELEM_MATCH));
+    }
 
-	protected boolean gt(Object object) throws QueryMatchingException {
-		throw new QueryMatchingException(getExceptionMessage(QueryOperator.$GT));
-	}
+    protected boolean gt(Object object) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$GT));
+    }
 
-	protected boolean gte(Object object) throws QueryMatchingException {
-		throw new QueryMatchingException(getExceptionMessage(QueryOperator.$GTE));
-	}
+    protected boolean gte(Object object) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$GTE));
+    }
 
-	protected boolean in(Object object) throws QueryMatchingException {
-		throw new QueryMatchingException(getExceptionMessage(QueryOperator.$IN));
-	}
+    protected boolean in(Object object) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$IN));
+    }
 
-	protected boolean like(Object object) throws QueryMatchingException {
-		throw new QueryMatchingException(getExceptionMessage(QueryOperator.$LIKE));
-	}
+    protected boolean nin(Object object) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$NIN));
+    }
 
-	protected boolean lt(Object object) throws QueryMatchingException {
-		throw new QueryMatchingException(getExceptionMessage(QueryOperator.$LT));
-	}
+    protected boolean like(Object object) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$LIKE));
+    }
 
-	protected boolean lte(Object object) throws QueryMatchingException {
-		throw new QueryMatchingException(getExceptionMessage(QueryOperator.$LTE));
-	}
+    protected boolean lt(Object object) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$LT));
+    }
 
-	protected boolean ne(Object object) throws QueryMatchingException {
-		throw new QueryMatchingException(getExceptionMessage(QueryOperator.$NE));
-	}
+    protected boolean lte(Object object) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$LTE));
+    }
 
-	private String getExceptionMessage(QueryOperator queryOperator) throws QueryMatchingException {
-		return "Operator " + queryOperator.name() + " not suported for matching with literal "
-				+ literal.getClass().getName();
-	}
+    protected boolean ne(Object object) throws QueryMatchingException {
+        throw new QueryMatchingException(getExceptionMessage(QueryOperator.$NE));
+    }
+
+    private String getExceptionMessage(QueryOperator queryOperator) throws QueryMatchingException {
+        return "Operator " + queryOperator.name() + " not suported for matching with literal " + literal.getClass().getName();
+    }
 
 }
